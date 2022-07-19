01b2fbe90c017a8e12f6c58ddf210695b5c154a2
avinxxsh <52997831+avinxxsh@users.noreply.github.com>
2022-07-19 15:59:44 +0530

    Update challengeOne.py

--- challengeOne.py
+++ challengeOne.py
@@ -1,12 +1,9 @@
 import requests
 
-get_endpoint = 'https://bfhldevapigw.healthrx.co.in/campus-hiring/input?id=RollNo'
-params = {'id': 19070122039}
+get_endpoint = 'api_here'
+params = {}
 
 r = requests.get(get_endpoint, params=params)
 print(r.text)
 
-# data = r.content
 
-# post_endpoint = 'https://bfhldevapigw.healthrx.co.in/campus-hiring/submit?id=RollNo'
-# headers = {''}
